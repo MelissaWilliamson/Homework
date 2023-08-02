13e44b985e60df70bc203c075ce0e763c937c24c
Melissa Williamson <melissawilliamson123@outlook.com>
2023-08-02 12:16:09 +0100

    study commit

---
+++ Own Work/Codewars.py
@@ -0,0 +1,13 @@
+#def greet():
+#    print("hello world")
+#greet()
+import numbers
+
+list = ['1', '2', '3', '4']
+list2 = list.reverse()
+
+def reverse_list():
+   input(list)
+   print(list2)
+
+reverse_list()
---
+++ Own Work/gsfgs.py
@@ -0,0 +1,6 @@
+s=['1','2','3','4']
+
+def reversed_list(s):
+     return s.reverse()
+
+reversed_list(s)
